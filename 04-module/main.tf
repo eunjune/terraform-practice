1f7a092a2fd89c42af0a88249b70914fa431161b
eunjun <wm10418@worksmobile.com>
2023-04-04 08:56:07 +0900

    HCL count

--- 04-module/main.tf
+++ 04-module/main.tf
@@ -52,6 +52,41 @@ output "subnet_groups" {
   }
 }
 
+resource "aws_iam_user" "user_1" {
+  name = "user-1"
+}
+
+resource "aws_iam_user" "user_2" {
+  name = "user-2"
+}
+
+resource "aws_iam_user" "user_3" {
+  name = "user-3"
+}
+
+output "user_arns" {
+  value = [
+    aws_iam_user.user_1.arn,
+    aws_iam_user.user_2.arn,
+    aws_iam_user.user_3.arn,
+  ]
+}
+
+/*
+ * count
+ */
+
+resource "aws_iam_user" "count" {
+  count = 10 # 몇 개 생성할지 지정. meta-argument : resource 타입과 관계 없이 지원해주는 속성
+
+  name = "count-user-${count.index}" # 0~9
+}
+
+output "count_user_arns" {
+  value = aws_iam_user.count.*.arn # 전체 유저
+}
+
+
 module "vpc" {
   source  = "tedilabs/network/aws//modules/vpc"
   version = "0.24.0"

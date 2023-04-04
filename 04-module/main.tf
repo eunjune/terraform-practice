aecfac1ed7f8ecef3164fd7d8f9564880d522bcf
eunjun <wm10418@worksmobile.com>
2023-04-04 10:16:49 +0900

    HCL for each

--- 04-module/main.tf
+++ 04-module/main.tf
@@ -86,6 +86,50 @@ output "count_user_arns" {
   value = aws_iam_user.count.*.arn # 전체 유저
 }
 
+/*
+ * for_each
+    set, map 구조
+ */
+
+resource "aws_iam_user" "for_each_set" {
+  for_each = toset([
+    "for-each-set-user-1",
+    "for-each-set-user-2",
+    "for-each-set-user-3",
+  ])
+
+  name = each.key
+}
+
+output "for_each_set_user_arns" {
+  value = values(aws_iam_user.for_each_set).*.arn
+}
+
+resource "aws_iam_user" "for_each_map" {
+  for_each = {
+    alice = {
+      level = "low"
+      manager = "posquit0"
+    }
+    bob = {
+      level = "mid"
+      manager = "posquit0"
+    }
+    john = {
+      level = "high"
+      manager = "steve"
+    }
+  }
+
+  name = each.key
+
+  tags = each.value
+}
+
+output "for_each_map_user_arns" {
+  value = values(aws_iam_user.for_each_map).*.arn
+}
+
 
 module "vpc" {
   source  = "tedilabs/network/aws//modules/vpc"

da64bc3992859773390dd32eaa9d7b1bb1e8d948
eunjun <eun-jun.jeong@worksmobile.com>
2023-04-06 08:14:20 +0900

    HCL 조건문

--- 04-module/main.tf
+++ 04-module/main.tf
@@ -10,6 +10,41 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+/*
+ * Conditional Expression
+ * Condtion ? If_True : If_False
+ */
+variable "is_john" {
+  type = bool
+  default = true
+}
+
+locals {
+  message = var.is_john ? "Hello John!" : "Hello!"
+}
+
+output "message" {
+  value = local.message
+}
+
+/*
+ * Count Trick for Conditional Resource
+ */
+variable "internet_gateway_enabled" {
+  type = bool
+  default = true
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "this" {
+  count = var.internet_gateway_enabled ? 1 : 0
+
+  vpc_id = aws_vpc.this.id
+}
+
 # 값을 정의하지 않으면 terraform apply 할 때 변수 주입하라고 나옴
 variable "vpc_name" {
   description = "생성되는 VPC 이름"
